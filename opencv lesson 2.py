d8f26c686a3aff69e603b59559299fbd499763bd
Tizoniara <55073546+Tizoniara@users.noreply.github.com>
2021-09-10 18:07:19 +1000

    Update opencv lesson 2.py

--- opencv lesson 2.py
+++ opencv lesson 2.py
@@ -33,27 +33,3 @@ cv2.imshow('final', blankImage)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-# frame = cv2.imread ('..\Photos/collage.png')
-# edges = cv2.Canny(frame,100,200) # This uses the canny edge detector. The 100 and 200 are rather arbitrary parameters; the second should be larger than the first, play around to see what numbers work best for each image.
-
-# contours, hierarchy = cv2.findContours(edges,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
-# # Get rid of the ones with an area smaller than tiny
-
-# #edges.shape returns a tuple containing width and height
-# blankImage = np.zeros(edges.shape)
-
-# goodContours=[]
-# for contour in contours:
-#     #check if the contour area is greater than 100 non zero pixels
-#     if cv2.contourArea(contour)>100:
-#         goodContours.append(contour)
-#         #redraw our good contours onto our blank canvas
-#         #isClosed = is our contour a closed loop
-#         #color = white
-#         cv2.polylines(blankImage,contour,isClosed=True,color=(255),thickness=1)
-
-# cv2.imshow("original", frame)
-# cv2.imshow("edges", edges) 
-# cv2.imshow("big contours", blankImage) 
-# cv2.waitKey(-1)
